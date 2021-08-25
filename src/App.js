d9d2c7c1968f164d15c193f2d57d22af52cc5cb4
Phebesue <62977906+Phebesue@users.noreply.github.com>
2021-08-25 19:29:45 -0400

    10.5 - LOGIN/LOGOUT

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Sitebar from './home/Navbar';
 import Auth from './auth/Auth';
+import WorkoutIndex from './workouts/WorkoutIndex';
 
 
 function App() {
@@ -16,18 +17,25 @@ function App() {
   const updateToken = (newToken) => {
     localStorage.setItem('token', newToken);
     setSessionToken(newToken);
-    console.log(sessionToken);
+    console.log("sessionToken: ",newToken); //altered, different from modules
   }
 
   const clearToken = () => {
     localStorage.clear();
     setSessionToken('');
+    console.log("sessionToken: ",sessionToken); //added, not in modules
   }
+
+  const protectedViews = ()=> {
+    return(sessionToken === localStorage.getItem('token') ? <WorkoutIndex token={sessionToken}/>
+      :  <Auth updateToken={updateToken}/>) 
+  }
+  
   
   return (
     <div>   
       <Sitebar clickLogout={clearToken} /> 
-      <Auth updateToken={updateToken} />
+    {protectedViews()}
     </div>
   );
 }
